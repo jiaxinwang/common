2339f7e98f3e596c66da00c64d38eb5efa1c4881
jiaxinwang <jiaxinwang@me.com>
2020-07-13 01:28:28 +0800

    + foreign of model

--- lazy/reflect.go
+++ lazy/reflect.go
@@ -15,6 +15,24 @@ func valueOfTag(inter interface{}, tagName string) interface{} {
 			}
 		}
 	}
-
 	return nil
+}
+
+func foreignOfModel(inter interface{}) [][3]string {
+	ret := make([][3]string, 0)
+	t := reflect.TypeOf(inter)
+	v := reflect.ValueOf(inter)
+	val := reflect.Indirect(reflect.ValueOf(inter))
+	for i := 0; i < v.NumField(); i++ {
+		if v.Field(i).CanInterface() {
+			tag := t.Field(i).Tag.Get("lazy")
+			if len(tag) > 0 {
+				if _, table, id, err := disassembleTag(tag); err == nil && len(table) != 0 && len(id) != 0 {
+					f := [3]string{val.Type().Field(i).Name, table, id}
+					ret = append(ret, f)
+				}
+			}
+		}
+	}
+	return ret
 }
