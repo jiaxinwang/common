b86bd47f1d50b3a41a2ee24934936f6417891c56
jiaxinwang <jiaxinwang@me.com>
2020-11-18 14:23:32 +0800

    + cp

--- fs/file.go
+++ fs/file.go
@@ -1,10 +1,37 @@
-package common
+package fs
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"os"
 )
+
+// Copy ...
+func Copy(src, dst string) error {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+	_, err = io.Copy(destination, source)
+	return err
+}
 
 // FileExist ...
 func FileExist(name string) bool {
